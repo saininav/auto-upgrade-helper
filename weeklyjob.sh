5ed8594371efe36924875b7bcb89a4f7e85b8d83
Marius Avram <marius.avram@intel.com>
2014-07-18 14:42:54 +0300

    Added script for cronjob
    Signed-off-by: Marius Avram <marius.avram@intel.com>

---
+++ weeklyjob.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Cronjob which can be run weekly to run the upgrade helper script.
+# Add the job in /etc/crontab like below.
+#
+# It will execute weekly at the same hour (8 AM).
+#
+# 00 8   * * 6   marius  /home/marius/work/yocto_package_upgrade_helper-main/weeklyjob.sh
+
+BUILDIR=/media/SSD/build2/
+POKYSOURCE=/home/marius/work/poky_http/poky/oe-init-build-env
+UPGRADESCRIPT=/home/marius/work/yocto_package_upgrade_helper-main/upgradehelper.py
+LOGFILE=/home/marius/work/yocto_package_upgrade_helper-main/upgradehelper.log
+
+cd $BUILDIR
+source $POKYSOURCE $BUILDIR
+python $UPGRADESCRIPT all &> $LOGFILE
